efa5ae41d1e887cd519030073814977fc4b16c9b
AmandaGroulx <>
2021-05-17 17:07:30 +0000

    added space

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ document.getElementById('enter').addEventListener('click', enterAge)
 // Creates a variable named "age" and sets it to 0.
 let age = 0
 
-  function enterAge () {
-// Gets the age that the user inputs and stores it in the variable 'age'. Sets it to an integer
+function enterAge() {
+  // Gets the age that the user inputs and stores it in the variable 'age'. Sets it to an integer
   age = document.getElementById('age').value
   age = parseInt(age)
-
   // Decides if the user is old enough to play the game and places them in an age category.
-
-    if (age >= 14) {
-  document.getElementById('text').innerHTML = 'Looks like youre old enough to play this game! Click on the picture to get to the website!'
+ if (age >= 14) {
+    document.getElementById('text').innerHTML = 'Looks like youre old enough to play this game! Click on the picture to get to the website!'
   }
-  
   else if (age >= 0) {
-  document.getElementById('text').innerHTML = 'Looks like youre not old enough to play this game yet, please look for a different game!'
+    document.getElementById('text').innerHTML = 'Looks like youre not old enough to play this game yet, please look for a different game!'
   }
 }
